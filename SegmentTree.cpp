20667bba1d8b7fdf5d7c7d485de0fcfd24f80722
GafrikJr <gafrikjr22848@gmail.com>
2025-05-23 17:40:48 +0300

    Update SegmentTree.cpp

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -27,15 +27,9 @@ void SegmentTree::update(int index, int value) {
 
 void SegmentTree::add(int index, int value) {
     vector<int> newArray(array.size() + 1);
-    for (int i = 0; i < index; i++)
-    {
-        newArray[i] = array[i];
-    }
+    for (int i = 0; i < index; i++) newArray[i] = array[i];
     newArray[index] = value;
-    for (int i = index; i < array.size(); i++)
-    {
-        newArray[i + 1] = array[i];
-    }
+    for (int i = index; i < array.size(); i++) newArray[i + 1] = array[i];
     array = newArray;
     tree = fillTree();
     minTree = fillMinTree();
